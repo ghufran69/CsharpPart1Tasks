4ce07d0bcd0d371fa992ceb49ec29a832b51e15b
ghufran <22ghufran2002@gmail.com>
2026-01-04 12:25:15 +0400

    Add Task3

--- Program.cs
+++ Program.cs
@@ -28,23 +28,48 @@ namespace CsharpProgram11
 
 
             //Task 2: Even or Odd
-            while (true)
+            //            while (true)
+            //            {
+            //                Console.WriteLine("Enter the numbers you want to check");
+            //                float number = float.Parse(Console.ReadLine());
+            //                if (number % 2 == 0)
+            //                {
+            //                    Console.WriteLine("the number is Even");
+
+            //                }
+            //                else
+            //                {
+            //                    Console.WriteLine("the number is Odd");
+
+            //                }
+            //                Console.WriteLine();
+
+            //            }
+            //        }
+            //    }
+            //}
+
+            Console.WriteLine("How many numbers do you want to enter");
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+
             {
-                Console.WriteLine("Enter the numbers you want to check");
-                float number = float.Parse(Console.ReadLine());
-                if (number % 2 == 0)
+                Console.WriteLine("enter the number");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+                int min = numbers[0];
+            for (int i = 0; i < n; i++)
+            {
+                if (numbers[i] < min)
                 {
-                    Console.WriteLine("the number is Even");
+                    min = numbers[i];
 
                 }
-                else
-                {
-                    Console.WriteLine("the number is Odd");
-
-                }
-                Console.WriteLine();
+            }
+            Console.WriteLine("the smallest number is :" + min);
 
             }
         }
     }
-}
+
