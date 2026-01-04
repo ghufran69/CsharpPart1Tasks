0276def1629be3ccca164e42d068d530aae3a4df
ghufran <22ghufran2002@gmail.com>
2026-01-04 09:03:58 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CsharpProgram11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many numbers do you want to enter?");
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("enter the number");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            int total = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                total += numbers[i];
+            }
+            float average = (float)total / n;
+            Console.WriteLine("The average is: " + average);
+        }
+    }
+}
+
+
+
